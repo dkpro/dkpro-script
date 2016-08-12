758215db46f9640a34d5be5f356485f8abae9fe8
Richard Eckart de Castilho <richard.eckart@gmail.com>
2016-08-12 17:18:01 +0200

    No issue. Switch to DKPro Core 1.8.0 and move manifest files.

--- dkpro-script-groovy/src/main/groovy/org/dkpro/script/groovy/internal/PipelineContext.groovy
+++ dkpro-script-groovy/src/main/groovy/org/dkpro/script/groovy/internal/PipelineContext.groovy
@@ -17,32 +17,32 @@
 // ****************************************************************************
 package org.dkpro.script.groovy.internal
 
-import groovy.grape.Grape;
-import groovy.json.*;
+import groovy.grape.Grape
+import groovy.json.*
 
-import static org.apache.uima.fit.factory.AnalysisEngineFactory.*;
-import static org.apache.uima.fit.factory.CollectionReaderFactory.*;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.*
+import static org.apache.uima.fit.factory.CollectionReaderFactory.*
 
 class PipelineContext
 {
-	def pipeline = [];
+	def pipeline = []
 
-	def engines;
+	def engines
 
-	def formats;
+	def formats
 
-	def VERSION = '1.7.0';
+	def VERSION = '1.8.0'
 
-	Script scriptContext;
+	Script scriptContext
 
-	ClassLoader classLoader;
+	ClassLoader classLoader
 
 	def version(ver) {
-		VERSION = ver;
+		VERSION = ver
 	}
 
 	def boot() {
-		java.util.logging.LogManager.logManager.reset(); // Disable logging
+		java.util.logging.LogManager.logManager.reset() // Disable logging
 		/*
 		 java.util.logging.LogManager.logManager.getLogger("").level = java.util.logging.Level.FINEST;
 		 java.util.logging.LogManager.logManager.getLogger("").handlers.each {
@@ -51,11 +51,13 @@ class PipelineContext
 		 */
 		//		
 
-		engines = new JsonSlurper().parse(this.getClass().getResourceAsStream("/PipelineContextJSON/engines.json"));
-		formats  = new JsonSlurper().parse(this.getClass().getResourceAsStream("/PipelineContextJSON/formats.json"))
+		engines = new JsonSlurper().parse(getClass().getResourceAsStream(
+            "/org/dkpro/script/groovy/manifest/engines-1.8.0.json"))
+		formats  = new JsonSlurper().parse(getClass().getResourceAsStream(
+            "/org/dkpro/script/groovy/manifest/formats-1.8.0.json"))
 	}
 
-	def lazyBootComplete = false;
+	def lazyBootComplete = false
 
 	// Thinks to initialize while the script is running, e.g. if we want to change the versio in
 	// the script, then we cannot configure the resolved before
@@ -73,63 +75,63 @@ class PipelineContext
 						root:'http://zoidberg.ukp.informatik.tu-darmstadt.de/artifactory/public-snapshots')
 			}
 
-			lazyBootComplete = true;
+			lazyBootComplete = true
 		}
 	}
 
 	def load(component) {
-		lazyBoot();
+		lazyBoot()
 
-		def cl = findClassLoader();
-		def desc;
-		def impl;
+		def cl = findClassLoader()
+		def desc
+		def impl
 		if (component.endsWith("Reader")) {
-			def format = formats[component[0..-7]];
+			def format = formats[component[0..-7]]
 			Grape.grab(group:format.groupId, module:format.artifactId, version: VERSION,
-			classLoader: cl);
-			impl = cl.loadClass(format.readerClass, true, false);
-			desc = createReaderDescription(impl);
+			classLoader: cl)
+			impl = cl.loadClass(format.readerClass, true, false)
+			desc = createReaderDescription(impl)
 		}
 		else if (component.endsWith("Writer")) {
-			def format = formats[component[0..-7]];
+			def format = formats[component[0..-7]]
 			Grape.grab(group:format.groupId, module:format.artifactId, version: VERSION,
-			classLoader: cl);
-			impl = cl.loadClass(format.writerClass, true, false);
-			desc = createEngineDescription(impl);
+			classLoader: cl)
+			impl = cl.loadClass(format.writerClass, true, false)
+			desc = createEngineDescription(impl)
 		}
 		else {
-			def engine = engines[component];
+			def engine = engines[component]
 			Grape.grab(group:engine.groupId, module:engine.artifactId, version: VERSION,
-			classLoader: findClassLoader());
-			impl = cl.loadClass(engine.class, true, false);
-			desc = createEngineDescription(impl);
+			classLoader: findClassLoader())
+			impl = cl.loadClass(engine.class, true, false)
+			desc = createEngineDescription(impl)
 		}
 
-		def comp = new Component();
-		comp.name = component;
-		comp.desc = desc;
-		comp.impl = impl;
-		return comp;
+		def comp = new Component()
+		comp.name = component
+		comp.desc = desc
+		comp.impl = impl
+		return comp
 	}
 
 	def findClassLoader() {
-		def cl;
+		def cl
 		if (classLoader) {
-			cl = classLoader;
+			cl = classLoader
 		}
 		else if (scriptContext) {
 			// When setting the parentClassLoader property, then UIMA no longer has access to the
 			// classes defined in the script, e.g. to closures!
 			if (scriptContext.binding.variables.get("parentClassLoader")) {
-				cl = scriptContext.binding.variables.get("parentClassLoader");
+				cl = scriptContext.binding.variables.get("parentClassLoader")
 			}
 			else {
-				cl = scriptContext.metaClass.theClass.classLoader;
+				cl = scriptContext.metaClass.theClass.classLoader
 			}
 		}
 		else {
-			cl = this.class.classLoader;
+			cl = this.class.classLoader
 		}
-		return cl;
+		return cl
 	}
 }
