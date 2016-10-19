70d69c1cc5367165c1b9698f55e083b04b1bca09
Richard Eckart de Castilho <richard.eckart@gmail.com>
2016-10-19 13:47:27 +0200

    No issue. Update license headers.

--- dkpro-script-groovy/src/main/groovy/org/dkpro/script/groovy/DKProScriptBase.groovy
+++ dkpro-script-groovy/src/main/groovy/org/dkpro/script/groovy/DKProScriptBase.groovy
@@ -1,20 +1,3 @@
-// ****************************************************************************
-// Copyright 2015
-// Ubiquitous Knowledge Processing (UKP) Lab
-// Technische Universität Darmstadt
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//   http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// ****************************************************************************
 package org.dkpro.script.groovy;
 
 import org.dkpro.script.groovy.internal.EngineHelper;
@@ -28,6 +11,23 @@ import static org.apache.uima.fit.pipeline.SimplePipeline.*;
 
 import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+/*
+ * Licensed to the Technische Universität Darmstadt under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The Technische Universität Darmstadt 
+ * licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.
+ *  
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 abstract class DKProCoreScript extends DelegatingScript {
     abstract void scriptBody()
